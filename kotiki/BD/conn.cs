e0985635db0d5413a0771eec5527a0e96795642e
IlnazFarkhullin <90451035+IlnazFarkhullin@users.noreply.github.com>
2022-04-20 14:57:36 +0300

    Добавьте файлы проекта.

---
+++ kotiki/BD/conn.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kotiki.BD
+{
+    class conn
+    {
+        public static kotikEntities entities = new kotikEntities();
+    }
+}
