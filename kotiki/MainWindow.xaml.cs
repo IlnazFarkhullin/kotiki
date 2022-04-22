355cdf34d28156a09b88510ba082d12264ca61f7
farhullin2017@gmail.com <90451035+IlnazFarkhullin@users.noreply.github.com>
2022-04-22 08:01:25 +0300

    почти конец

--- kotiki/MainWindow.xaml.cs
+++ kotiki/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using kotiki.BD;
 
+
 namespace kotiki
 {
     /// <summary>
@@ -21,10 +22,16 @@ namespace kotiki
     /// </summary>
     public partial class MainWindow : Window
     {
+        public int ItemCount { get; set; }
+        public int PCount { get;  set; }
+
         public MainWindow()
         {
             InitializeComponent();
             kots.ItemsSource = conn.entities.info.ToList();
+            refresh();
+            
+            
         }
 
         private void one_Click(object sender, RoutedEventArgs e)
@@ -40,6 +47,77 @@ namespace kotiki
         private void Search(object sender, TextChangedEventArgs e)
         {
             kots.ItemsSource = conn.entities.info.ToList().Where(z => z.name.Contains(search.Text)); //поиск по имени
+        }
+
+        public void Button_Click()
+        {
+            StackBTN.Children.Clear();
+            var btnB = new Button();
+            btnB.Content = "←";
+            btnB.Click += btnC_Click;
+            StackBTN.Children.Add(btnC);
+            for (int i = 1; i < ItemCount; i+=10)
+            {
+                var btnC = new Button();
+                btnC.Content = ((i / 10) + 1).ToString();
+                btnC.Click += btnC_Click;
+                StackBTN.Children.Add(btnC);
+            }
+            var btnN = new Button();
+            btnN.Content = "→";
+            btnN.Click += btnN_Click;
+            StackBTN.Children.Add(btnN);
+            TextBlock textP = new TextBlock();
+            if ((ItemCount +11)>PCount)
+            {
+                textP.Text = ItemCount + "\\" + ItemCount;
+            }
+            else
+            {
+                textP.Text = ItemCount + "\\" + ItemCount;
+            }
+            StackBTN.Children.Add(textP);
+        }
+
+        public  void refresh()
+        {
+            kots.ItemsSource = conn.entities.info.ToList().Skip(1).Take(10);
+            Button_Click();
+        }
+
+        public  void refr()
+        {
+            kots.ItemsSource = conn.entities.info.ToList().Skip(10).Take(1);
+            
+        }
+
+
+        private void btnC_Click(object sender, RoutedEventArgs e)
+        {
+            var CBtn = (sender as Button).Content;
+            PCount = ((Convert.ToInt32(CBtn) + 1) * 10);
+            refresh();
+
+           
+        }
+        private void btnB_Click(object sender, RoutedEventArgs e)
+        {
+            if ((ItemCount-10)<PCount)
+            {
+                ItemCount -= 10;
+            }
+            
+            refresh();
+        }
+
+        private void btnN_Click(object sender, RoutedEventArgs e)
+        {
+            if ((ItemCount+10)< PCount)
+            {
+                ItemCount += 10;
+            }
+            refr();
+           
         }
     }
 }
